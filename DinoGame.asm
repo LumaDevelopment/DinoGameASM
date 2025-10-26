9a401e3ab13a01d9f9aacc5cb094713c334a3167
LumaDevelopment <12071794+LumaDevelopment@users.noreply.github.com>
2025-10-26 16:09:14 -0400

    Wrote the `ConsoleSizePrompt` procedure. This'll be called when the game is started, to ensure the console is the right size for gameplay.

--- DinoGame.asm
+++ DinoGame.asm
@@ -8,7 +8,10 @@ TARGET_COLS = 120
 
 .data
 
+consoleSizeDNMMsg      BYTE "Console size (Rows, Col) does not match target!",0
 consoleSizeTargetName  BYTE "Target",0
+consoleSizeActualName  BYTE "Actual",0
+consoleSizePromptMsg   BYTE "Please resize the console window and press any key to continue!",0
 
 printCoordinatePrefix BYTE ": (",0 ; Text printed after the label and before X
 printCoordinateSep    BYTE ", ",0  ; Text printed between X and Y
@@ -60,9 +63,48 @@ PrintCoordinate PROC USES eax edx,
 	ret
 PrintCoordinate ENDP
 
+; Holds the user until they resize the console 
+; to the target size.
+ConsoleSizePrompt PROC USES eax ebx edx
+	CheckConsoleSize:
+		call VerifyConsoleSize
+		cmp  bl, 1
+		je   CorrectConsoleSize ; Console size is correct
+
+		; Notify user that console size does not match
+		mov  edx, OFFSET consoleSizeDNMMsg
+		call WriteString
+		call Crlf
+
+		; Print target console size
+		INVOKE PrintCoordinate, ADDR consoleSizeTargetName, TARGET_ROWS, TARGET_COLS
+
+		; Move rows and cols to EAX and EBX
+		; so INVOKE does not have to widen them
+		Call  GetMaxXY
+		movzx eax, al
+		movzx ebx, dl
+
+		; Print actual console size
+		INVOKE PrintCoordinate, ADDR consoleSizeActualName, eax, ebx
+
+		; Prompt the user to resize the console
+		mov  edx, OFFSET consoleSizePromptMsg
+		call WriteString
+		call Crlf
+		call Crlf
+
+		; Wait for user to press a key, then try again
+		call ReadChar
+		jmp CheckConsoleSize
+
+	CorrectConsoleSize:
+		call Clrscr ; In case any prompts were put on the screen
+		ret
+ConsoleSizePrompt ENDP
+
 main PROC
-	; Test PrintCoordinate
-	INVOKE PrintCoordinate, ADDR consoleSizeTargetName, TARGET_ROWS, TARGET_COLS
+	call ConsoleSizePrompt
 
 	exit
 main ENDP
