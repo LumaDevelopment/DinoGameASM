76dccb2a9f968c2b394eaeaaebcb7a46a9e7f9f0
LumaDevelopment <12071794+LumaDevelopment@users.noreply.github.com>
2025-10-26 20:29:08 -0400

    Create `WipeRowInScreen` procedure. This'll be used in the new `WriteTerrain` procedure to wipe every row before the terrain layers are hit.

--- Screen.asm
+++ Screen.asm
@@ -13,6 +13,23 @@ blankRow     BYTE TARGET_COLS DUP(' '),0dh,0ah
 
 .code
 
+WipeRowInScreen PROC USES eax ecx edi esi,
+     rowIndex:BYTE ; Unsigned int representing the index of the row to wipe
+
+     ; Calculate offset
+     movzx eax,rowIndex
+     imul  eax,ROW_SIZE
+
+     ; Move bytes from blankRow pos to eax pos
+     cld
+     mov esi,OFFSET blankRow
+     lea edi,screenBuffer[eax]
+     mov ecx,ROW_SIZE
+     rep movsb
+
+     ret
+WipeRowInScreen ENDP
+
 ; PROC - WipeRowInScreen
 ; PROC - SetRowInScreen
 ; PROC - SetPixelInScreen
