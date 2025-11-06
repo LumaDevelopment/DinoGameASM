d994e3d1bafa3d24b263859f013e15ddc05d8306
LumaDevelopment <12071794+LumaDevelopment@users.noreply.github.com>
2025-11-05 22:25:18 -0500

    Implemented simple case of score calculation (endTime > startTime). Next need to implement complex calculation case, and case where startTime=endTime

--- Score.asm
+++ Score.asm
@@ -27,4 +27,27 @@ RecordEndTime PROC USES eax
      ret
 RecordEndTime ENDP
 
+; Calculate score based on start and end time.
+; Returns score in EAX.
+GetScore PROC USES ebx edx
+     mov eax,endTime
+     cmp startTime,eax
+     ; If startTime > endTime, then tick counter reset
+     ; to 0 while the user was playing, making 
+     ; calculations more complex
+     ja ComplexCalc
+
+     SimpleCalc:
+          ; Calculate difference between start and end time
+          mov eax,endTime
+          sub eax,startTime
+
+          ; Divide ms elapsed by 10 to get score
+          mov edx,0  ; Clear upper half of dividend
+          mov ebx,10 ; Load divisor
+          div ebx
+     ComplexCalc:
+     ret
+GetScore ENDP
+
 END
