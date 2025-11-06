bd9469d1b30127876a4b19020223cd73fce574ad
LumaDevelopment <12071794+LumaDevelopment@users.noreply.github.com>
2025-11-05 23:48:30 -0500

    Made skeleton of 'AreBoundingBoxesColliding' procedure. Split BoundingBox into Coordinate and BoundingBox, and moved to include file.

--- Physics.asm
+++ Physics.asm
@@ -3,13 +3,22 @@
 
 INCLUDE DinoGame.inc
 
-; The physical representation of a given sprite.
-; Used for collision calculations
-BoundingBox STRUCT
-     x         BYTE ?
-     y         BYTE ?
-     boxWidth  BYTE ?
-     boxHeight BYTE ?
-BoundingBox ENDS
+.code
+
+; Determines if two bounding boxes are colliding.
+; Algorithm stolen from:
+; https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection#axis-aligned_bounding_box
+; Returns: BL = 1 if colliding, 0 otherwise
+AreBoundingBoxesColliding PROC,
+     boxOne:BoundingBox,
+     boxTwo:BoundingBox
+
+     ; boxOne.Position.X < boxTwo.Position.X + boxTwo.BoxWidth
+     ; boxOne.Position.X + boxOne.BoxWidth > boxTwo.Position.X
+     ; boxOne.Position.Y < boxTwo.Position.Y + boxTwo.BoxHeight
+     ; boxOne.Position.Y + boxOne.BoxHeight > boxTwo.Position.Y
+
+     ret
+AreBoundingBoxesColliding ENDP
 
 END
